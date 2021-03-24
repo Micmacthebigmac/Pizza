3e0f62c883534a8dc9ac44fc38c577aedefc5ee9
Hervé <h.richard.codeur.onlinem>
2021-03-24 16:20:23 +0100

    Add Background 01 & 02

--- index.php
+++ index.php
@@ -1,12 +1,15 @@
 <?php include 'header.php'?>
 
 
-<nav class="navbar navbar-light bg-light">
-  <div class="container">
-    <a class="navbar-brand" href="#">
-      <img src="photo/facebook-noir.png" alt="" width="30" height="24">
-    </a>
-  </div>
-</nav>
+<header>
+
+    <img src="photo/background-01.png" alt="background" class="background01">
+    <img src="photo/background-02.png" alt="background" class="background02">
+    <img src="photo/6 1.png.png" alt="background" class="background6_1">
+    <img src="photo/6 2.png.png" alt="background" class="background6_2">
+
+
+</header>
+
 
 <?php include 'footer.php'?>
