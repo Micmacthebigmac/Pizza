30e5b843e3e90aeb32d1ed8c58562e18dca91b67
Hervé <h.richard.codeur.onlinem>
2021-03-26 09:03:02 +0100

    update

--- index.php
+++ index.php
@@ -1,15 +1,48 @@
 <?php include 'header.php'?>
 
 
-<header>
+<header id="header" class=" p-5">
 
-    <img src="photo/background-01.png" alt="background" class="background01">
-    <img src="photo/background-02.png" alt="background" class="background02">
-    <img src="photo/6 1.png.png" alt="background" class="background6_1">
-    <img src="photo/6 2.png.png" alt="background" class="background6_2">
+    <p><img src="photo/Italian Cuisine.png" alt="blabla" class="italianCuisine"></p>
+
+
+<nav class="navbar navbar-expand-lg navbar-dark">
+  <div class="container-fluid">
+
+    <a class="navbar-brand" href="#">
+        <img src="photo/pizza.png" alt="" class="d-inline-block align-text-top">
+    </a>
+
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse"></div>
+
+    <div class="" id="navbarText">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Features</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Pricing</a>
+        </li>
+      </ul>
+    </div>
+    
+  </div>
+</nav>
 
 
 </header>
 
+    
+<p><img src="photo/texture-top.png" alt="background" class="background6_1"></p>
+    
+
+    <div class="d-flex justify-content-end"></div>
 
 <?php include 'footer.php'?>
