1aff1fcdff1c606543ef99a1fb3f23c9add79fa1
Hervé <h.richard.codeur.onlinem>
2021-04-01 16:33:30 +0200

    update

--- index.php
+++ index.php
@@ -1,16 +1,12 @@
 <?php include 'header.php'?>
 
 
-<header id="header" class=" p-5">
-
-    <p><img src="photo/Italian Cuisine.png" alt="blabla" class="italianCuisine"></p>
-
+<div class="container-fluid" id="first-section">
 
 <nav class="navbar navbar-expand-lg navbar-dark">
-  <div class="container-fluid">
 
     <a class="navbar-brand" href="#">
-        <img src="photo/pizza.png" alt="" class="d-inline-block align-text-top">
+        <img src="photo/pizza.png" alt="logo" class="d-inline-block align-text-top">
     </a>
 
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
@@ -33,16 +29,35 @@
       </ul>
     </div>
     
-  </div>
 </nav>
 
+</div>
 
-</header>
+<div class="italian">
+<h1>Italian<br />Cuisine</h1>
+<div class="blur">
+</div>
+</div>
+<div class="row-fdp2">
+<h2>Our specialites</h2>
+</div>
+
+<div class="row-fdp" id="ourSpecialities">
+  <div class="column-fdp">
+  <img src="photo/pizza-photo.png" alt="Pizza" class="photoPizza">
+</div>
+  <div class="column-fdp">
+  <h1 class="italianPicture">Italian<br />Pizza</h1>
+</div>
+</div>
+
+<div class="italian2">
+
+</div>
 
     
-<p><img src="photo/texture-top.png" alt="background" class="background6_1"></p>
-    
+      
 
-    <div class="d-flex justify-content-end"></div>
+
 
 <?php include 'footer.php'?>
